2771f1f394d16e3fc1afdd8e474426820371ef35
SWE-Yazeed <yazeed.alhariqi@gmail.com>
2025-09-03 12:21:58 +0300

    Add Jenkines

---
+++ src/main/java/com/wakeb/CalculatorApp.java
@@ -0,0 +1,13 @@
+package com.wakeb;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+        int x = Calculator.add(2,3);
+
+        int y = Calculator.subtract(2,3);
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ src/test/java/com/wakeb/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.wakeb;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+
+    @Test
+    public void testAddPositive(){
+        int sum = Calculator.add(4,7);
+
+        assertEquals(sum, 1);
+    }
+
+    @Test
+    public void testAddNegative(){
+        int sum = Calculator.add(-4,7);
+        assertEquals(sum, 3);
+    }
+
+    @Test
+    public void testSubtractPositive(){
+        int sum = Calculator.subtract(4,7);
+        assertEquals(sum, 1);
+    }
+}
+
+
